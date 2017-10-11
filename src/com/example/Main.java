e4124ade6541f1a85865a9873cfa745033ab7d0a
Brian Sheen <bsheen@gmail.com>
2017-10-11 15:56:31 -0500

    solved reverse integer

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,6 +3,28 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int x = 10021;
+        System.out.println(reverse(x));
+    }
+
+    public static Integer reverse(Integer x) {
+        String reverse;
+        if (x < 0) {
+            reverse = String.valueOf(Math.abs(x));
+        } else {
+            reverse = String.valueOf(x);
+        }
+        StringBuilder solution = new StringBuilder();
+        for (int i = reverse.length() - 1; i >= 0; --i) {
+            solution.append(reverse.charAt(i));
+        }
+        try {
+            if (x < 0) {
+                return -Integer.valueOf(solution.toString());
+            }
+            return Integer.valueOf(solution.toString());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
     }
 }
